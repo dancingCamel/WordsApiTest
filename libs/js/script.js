db94b676516d25fff65d09b2d6ba4f05b169e7eb
Robbm <robert.brindley.morgan+github@gmail.com>
2020-08-16 00:49:11 +0100

    Added curl and dynamic population of page.

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,34 +1,91 @@
-	$('#btnRun').click(function() {
+$('#searchBtn').click(function(event) {
+	event.preventDefault();
+	$('#resultsContainer').html("");
+	hideError();
+	showLoadingImg();
 
-		$.ajax({
-			url: "libs/php/getCountryInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				country: $('#selCountry').val(),
-				lang: $('#selLanguage').val()
-			},
-			success: function(result) {
-
-				console.log(result);
-
-				if (result.status.name == "ok") {
-
-					$('#txtContinent').html(result['data'][0]['continent']);
-					$('#txtCapital').html(result['data'][0]['capital']);
-					$('#txtLanguages').html(result['data'][0]['languages']);
-					$('#txtPopulation').html(result['data'][0]['population']);
-					$('#txtArea').html(result['data'][0]['areaInSqKm']);
-
-				}
+	$.ajax({
+		url: "libs/php/getWordInfo.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			word: $('#searchBox').val()
+		},
+		success: function(result) {
+			hideLoadingImg()
+			console.log(result);
 			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				console.log(errorThrown)
-				console.log(jqXHR)
-				console.log(textStatus)
-			}
-		}); 
-	
+			if (result.status.name == "ok") {
+				let data = result['data'];
+				let keys = Object.keys(data);
+				let word = result.word;
 
-	});
+				keys.forEach((key, index) => {
+
+					var result = document.createElement('div');
+					$(result).addClass('result');
+
+					var title = document.createElement('h1');
+					$(title).addClass('d-inline-block').addClass('text-capitalize');
+					title.appendChild(document.createTextNode(word))
+					result.appendChild(title);
+
+					var partOfSpeech = document.createElement('p');
+					$(partOfSpeech).addClass('text-muted').addClass('ml-3').addClass('d-inline-block');
+					partOfSpeech.appendChild(document.createTextNode(key))
+					result.appendChild(partOfSpeech);
+
+					var subTitle = document.createElement('h4');
+					subTitle.appendChild(document.createTextNode("Definition of "));
+					var italic = document.createElement('span');
+					$(italic).addClass('font-italic');
+					italic.appendChild(document.createTextNode(word));
+					subTitle.appendChild(italic);
+					result.appendChild(subTitle);
+
+					var definitions = document.createElement('div');
+					$(definitions).addClass('definitionContainer');
+					var count = 1
+					for(var i = 0; i < data[key].length; i++){
+						var definition = document.createElement('p');
+						definition.appendChild(document.createTextNode(count + ". " +data[key][i]));
+						definitions.appendChild(definition);
+						count++;
+					}
+					result.appendChild(definitions);
+
+					$('#resultsContainer').append(result);
+				});
+			}	
+			else {
+				showError(result.message);
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			hideLoadingImg()
+			showError(textStatus+ ": " + errorThrown);
+		},
+	}); 
+
+
+});
+
+// helper functions
+function showError(message){
+	console.log('error')
+	$('#errorAlert').html(message);
+	$('#errorAlert').show();
+}
+
+function hideError(){
+	$('#errorAlert').html("");
+	$('#errorAlert').hide();
+}
+
+function showLoadingImg(){
+	$('#spinnerContainer').show();
+}
+
+function hideLoadingImg(){
+	$('#spinnerContainer').hide();
+}
---
+++ libs/php/getWordInfo.php
@@ -0,0 +1,72 @@
+<?php
+	// ini_set('display_errors', 'On');
+	// error_reporting(E_ALL);
+	$executionStartTime = microtime(true) / 1000;
+
+	$ch = curl_init();
+
+	$url = "https://wordsapiv1.p.rapidapi.com/words/" . $_REQUEST['word'];
+	curl_setopt_array($ch, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+			"x-rapidapi-host: wordsapiv1.p.rapidapi.com",
+			"x-rapidapi-key: 625da2911bmsh57780aa52292aa2p1857fdjsn8b01e4caf2cf"
+		),
+	));
+
+	$result= curl_exec($ch);
+	$err = curl_error($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);	
+	$endtime = microtime(true) / 1000;
+
+	if ($err) {
+		$output['status']['name'] = "error";
+		$output['status']['code'] = "500";
+		$output['message'] = $err;
+		
+	} 
+	else {
+		if (array_key_exists('success', $decode)){
+			$output['status']['code'] = "404";
+			$output['status']['name'] = "error";
+			$output['message'] = $decode['message'];
+		
+		}
+		else {
+			$partsOfSpeech = array("adverb", "adjective", "noun", "pronoun", "verb");
+			$temp = array();
+
+			foreach($partsOfSpeech as $part){
+				foreach($decode['results'] as $entry){
+					if ($entry['partOfSpeech'] == $part) {
+						if (!array_key_exists($part, $temp)){
+							$temp[$part] = array();
+						}
+						array_push($temp[$part], $entry['definition']);
+					}
+				}
+				
+			}
+
+			$output['status']['code'] = "200";
+			$output['status']['name'] = "ok";
+			$output['status']['description'] = "success";
+			$output['status']['returnedIn'] = ($endtime - $executionStartTime);
+			$output['word'] = $decode['word'];
+			$output['data'] = $temp;
+
+		}
+		
+	}
+	header('Content-Type: application/json; charset=UTF-8');
+	echo json_encode($output); 
